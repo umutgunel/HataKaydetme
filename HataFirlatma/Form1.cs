429d7d1af9aa82fbd06f8b023e3692df8249e736
Umut GUNEL <umutgunel@hotmail.de>
2024-10-17 16:53:50 +0300

    Add project files.

---
+++ HataFirlatma/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HataFirlatma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Program.cs dosyasındaki Main metoduna 
+         * 
+         * Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+         * 
+         * Ekleniyor. Application_ThreadException metodu aşağıdaki gibi olacak;
+         * 
+         * private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+                {
+                    System.IO.File.AppendAllText("D:\\HataOku.txt",
+                        e.Exception.Message + " " +
+                        e.Exception.Source + " " +
+                        e.Exception.StackTrace + " " +
+                        "\r\n\r\n".ToString());
+                }
+         *          
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int i = 5;
+            int j = 0;
+            int x = i / j;
+        }
+    }
+}
---
+++ HataFirlatma/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HataFirlatma
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            Application.Run(new Form1());
+        }
+
+        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            System.IO.File.AppendAllText("D:\\HataOku.txt",
+                e.Exception.Message + " " +
+                e.Exception.Source + " " +
+                e.Exception.StackTrace + " " +
+                "\r\n\r\n".ToString());
+        }
+    }
+}
